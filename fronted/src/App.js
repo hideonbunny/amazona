a3c1b40bd65da22ea7d22a4e4450a7c89f670bcd
sherry <linting560@gmail.com>
2023-08-30 13:51:43 -0400

    first commit

--- fronted/src/App.js
+++ fronted/src/App.js
@@ -1,13 +1,25 @@
 import "./App.css";
+import data from "./data";
+import { BrowserRouter, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">amazona</a>
-      </header>
-      <main>list of products</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">amazona</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
